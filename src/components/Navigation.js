d0e5b769bd87163bdc9bce0c88ad14c0cd78fa33
Natalia <jdernt@gmail.com>
2020-10-21 11:36:41 +0400

    initial commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+// навигация: 
+//  1 кнопка перехода на страницу слайдера
+//  2 кнопка плеера
+//  3 кнопка браузера с переходом по указанной ссылке
+//  4 кнопка выхода
+
+import React from "react";
+import { StyleSheet, Text, View, AppRegistry } from "react-native";
+
+import { NativeRouter, Route, Link } from "react-router-native";
